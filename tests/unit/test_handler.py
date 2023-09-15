3bb9b854e0458a5e8b55f79ecfa07e4b9c9c6eaa
antonblazquezd <74876090+antonblazquezd@users.noreply.github.com>
2023-09-15 11:36:16 +0200

    get all assets test

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -12,6 +12,10 @@ UUID_MOCK_VALUE_JOHN = "f8216640-91a2-11eb-8ab9-57aa454facef"
 UUID_MOCK_VALUE_JANE = "31a9f940-917b-11eb-9054-67837e2c40b0"
 UUID_MOCK_VALUE_NEW_USER = "new-user-guid"
 
+ASSETS_MOCK_TABLE_NAME = "AssetsTest"
+UUID_MOCK_VALUE_BTC = "09d97f0a-23e9-4930-a93a-cba3c9b7e9e2"
+UUID_MOCK_VALUE_ETH = "79add0c4-4a41-4b5b-84a8-ad513242b390"
+
 
 def mock_uuid():
     return UUID_MOCK_VALUE_NEW_USER
@@ -33,6 +37,14 @@ def set_up_dynamodb():
             {"AttributeName": "userId", "KeyType": "HASH"},
         ],
         AttributeDefinitions=[{"AttributeName": "userId", "AttributeType": "S"}],
+        ProvisionedThroughput={"ReadCapacityUnits": 1, "WriteCapacityUnits": 1},
+    )
+    conn.create_table(
+        TableName=ASSETS_MOCK_TABLE_NAME,
+        KeySchema=[
+            {"AttributeName": "assetId", "KeyType": "HASH"},
+        ],
+        AttributeDefinitions=[{"AttributeName": "assetId", "AttributeType": "S"}],
         ProvisionedThroughput={"ReadCapacityUnits": 1, "WriteCapacityUnits": 1},
     )
 
@@ -59,6 +71,22 @@ def put_data_dynamodb():
             "lastName": {"S": "Doe"},
             "email": {"S": "janedoe@gmail.com"},
             "phone": {"S": "600000002"},
+        },
+    )
+    conn.put_item(
+        TableName=ASSETS_MOCK_TABLE_NAME,
+        Item={
+            "assetId": {"S": UUID_MOCK_VALUE_BTC},
+            "symbol": {"S": "BTC"},
+            "blockchain": {"S": "Bitcoin"},
+        },
+    )
+    conn.put_item(
+        TableName=ASSETS_MOCK_TABLE_NAME,
+        Item={
+            "assetId": {"S": UUID_MOCK_VALUE_ETH},
+            "symbol": {"S": "ETH"},
+            "blockchain": {"S": "Ethereum"},
         },
     )
 
@@ -162,3 +190,31 @@ def test_delete_user():
 
 
 # Add your unit testing code here
+
+
+@patch.dict(
+    os.environ,
+    {"ASSETS_TABLE": ASSETS_MOCK_TABLE_NAME, "AWS_XRAY_CONTEXT_MISSING": "LOG_ERROR"},
+)
+def test_get_list_of_assets():
+    with my_test_environment():
+        from src.api import assets
+
+        with open("./events/assets/event-get-all-assets.json", "r") as f:
+            apigw_get_all_assets_event = json.load(f)
+        expected_response = [
+            {
+                "assetId": {"S": UUID_MOCK_VALUE_BTC},
+                "symbol": {"S": "BTC"},
+                "blockchain": {"S": "Bitcoin"},
+            },
+            {
+                "assetId": {"S": UUID_MOCK_VALUE_BTC},
+                "symbol": {"S": "BTC"},
+                "blockchain": {"S": "Bitcoin"},
+            }
+        ]
+        ret = assets.lambda_handler(apigw_get_all_assets_event, "")
+        assert ret["statusCode"] == 200
+        data = json.loads(ret["body"])
+        assert data == expected_response
