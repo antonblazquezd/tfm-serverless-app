18b19d4d3a3e0daf95f3ee7ccefa8b05587f9eb4
antonblazquezd <74876090+antonblazquezd@users.noreply.github.com>
2023-09-19 22:25:55 +0200

    wallet tests finished

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -11,6 +11,16 @@ USERS_MOCK_TABLE_NAME = "UsersTest"
 ASSETS_MOCK_TABLE_NAME = "AssetsTest"
 WALLETS_MOCK_TABLE_NAME = "WalletsTest"
 
+UUID_MOCK_VALUE_JOHN = "f8216640-91a2-11eb-8ab9-57aa454facef"
+UUID_MOCK_VALUE_JANE = "31a9f940-917b-11eb-9054-67837e2c40b0"
+UUID_MOCK_VALUE_MARY = "756d5aa2-3f60-4ae8-a9c7-32079d55990d"
+
+UUID_MOCK_VALUE_BTC = "09d97f0a-23e9-4930-a93a-cba3c9b7e9e2"
+UUID_MOCK_VALUE_ETH = "79add0c4-4a41-4b5b-84a8-ad513242b390"
+UUID_MOCK_VALUE_DOT = "5bc3d175-513e-43fc-9edd-64c9f6de9b8e"
+
+UUID_MOCK_VALUE_NEW_WALLET1 = "358d3f25-1cab-471b-ae8d-453246849c19"
+UUID_MOCK_VALUE_NEW_WALLET2 = "55ddefa2-4723-4203-abe4-9390d79a45f2"
 
 @contextmanager
 def my_test_environment():
@@ -18,6 +28,7 @@ def my_test_environment():
         set_up_dynamodb()
         put_data_dynamodb_user()
         put_data_dynamodb_asset()
+        put_data_dynamodb_wallet()
         yield
 
 
@@ -38,6 +49,28 @@ def set_up_dynamodb():
         ],
         AttributeDefinitions=[{"AttributeName": "assetId", "AttributeType": "S"}],
         ProvisionedThroughput={"ReadCapacityUnits": 1, "WriteCapacityUnits": 1},
+    )
+    conn.create_table(
+        TableName=WALLETS_MOCK_TABLE_NAME,
+        KeySchema=[
+            {"AttributeName": "walletId", "KeyType": "HASH"},
+        ],
+        AttributeDefinitions=[
+            {"AttributeName": "walletId", "AttributeType": "S"},
+            {"AttributeName": "userId", "AttributeType": "S"},
+        ],
+        ProvisionedThroughput={"ReadCapacityUnits": 1, "WriteCapacityUnits": 1},
+        GlobalSecondaryIndexes=[
+            {
+                "IndexName": "Wallets-AssetIndex",
+                "KeySchema": [
+                    {"AttributeName": "userId", "KeyType": "HASH"},
+                ],
+                "Projection": {
+                    "ProjectionType": "ALL",
+                },
+            },
+        ],
     )
 
 
@@ -65,6 +98,17 @@ def put_data_dynamodb_user():
             "phone": {"S": "600000002"},
         },
     )
+    conn.put_item(
+        TableName=USERS_MOCK_TABLE_NAME,
+        Item={
+            "userId": {"S": UUID_MOCK_VALUE_MARY},
+            "idNumber": {"S": "00000000C"},
+            "firstName": {"S": "Mary"},
+            "lastName": {"S": "Doe"},
+            "email": {"S": "marydoe@gmail.com"},
+            "phone": {"S": "600000003"},
+        },
+    )
 
 
 def put_data_dynamodb_asset():
@@ -86,13 +130,45 @@ def put_data_dynamodb_asset():
         },
     )
 
+    conn.put_item(
+        TableName=ASSETS_MOCK_TABLE_NAME,
+        Item={
+            "assetId": {"S": UUID_MOCK_VALUE_DOT},
+            "symbol": {"S": "DOT"},
+            "blockchain": {"S": "Polkadot"},
+        },
+    )
+
+def put_data_dynamodb_wallet():
+    conn = boto3.client("dynamodb")
+
+    # create wallets
+    conn.put_item(
+        TableName=WALLETS_MOCK_TABLE_NAME,
+        Item={
+            "walletId": {"S": UUID_MOCK_VALUE_NEW_WALLET1},
+            "address": {"S": "0x123456789"},
+            "balance": {"S": "5"},
+            "userId": {"S": UUID_MOCK_VALUE_MARY},
+            "assetId": {"S": UUID_MOCK_VALUE_DOT},
+        },
+    )
+    conn.put_item(
+        TableName=WALLETS_MOCK_TABLE_NAME,
+        Item={
+            "walletId": {"S": UUID_MOCK_VALUE_NEW_WALLET2},
+            "address": {"S": "0x987654321"},
+            "balance": {"S": "10"},
+            "userId": {"S": UUID_MOCK_VALUE_MARY},
+            "assetId": {"S": UUID_MOCK_VALUE_DOT},
+        },
+    )
+
 
 # ----------------------------
 #           USERS
 # ----------------------------
 
-UUID_MOCK_VALUE_JOHN = "f8216640-91a2-11eb-8ab9-57aa454facef"
-UUID_MOCK_VALUE_JANE = "31a9f940-917b-11eb-9054-67837e2c40b0"
 UUID_MOCK_VALUE_NEW_USER = "new-user-guid"
 
 
@@ -126,6 +202,14 @@ def test_get_list_of_users():
                 "lastName": "Doe",
                 "email": "janedoe@gmail.com",
                 "phone": "600000002",
+            },
+            {
+                "userId": UUID_MOCK_VALUE_MARY,
+                "idNumber": "00000000C",
+                "firstName": "Mary",
+                "lastName": "Doe",
+                "email": "marydoe@gmail.com",
+                "phone": "600000003",
             },
         ]
         ret = users.lambda_handler(apigw_get_all_users_event, "")
@@ -202,10 +286,7 @@ def test_delete_user():
 #           ASSETS
 # ----------------------------
 
-UUID_MOCK_VALUE_BTC = "09d97f0a-23e9-4930-a93a-cba3c9b7e9e2"
-UUID_MOCK_VALUE_ETH = "79add0c4-4a41-4b5b-84a8-ad513242b390"
 UUID_MOCK_VALUE_NEW_ASSET = "new-asset-guid"
-
 
 def mock_uuid_asset():
     return UUID_MOCK_VALUE_NEW_ASSET
@@ -231,6 +312,11 @@ def test_get_list_of_assets():
                 "assetId": UUID_MOCK_VALUE_ETH,
                 "symbol": "ETH",
                 "blockchain": "Ethereum",
+            },
+            {
+                "assetId": UUID_MOCK_VALUE_DOT,
+                "symbol": "DOT",
+                "blockchain": "Polkadot",
             },
         ]
         ret = assets.lambda_handler(apigw_get_all_assets_event, "")
@@ -302,67 +388,20 @@ def test_delete_asset():
 # ----------------------------
 
 UUID_MOCK_VALUE_NEW_WALLET = "new-wallet-guid"
-UUID_MOCK_VALUE_NEW_WALLET1 = "358d3f25-1cab-471b-ae8d-453246849c19"
-UUID_MOCK_VALUE_NEW_WALLET2 = "55ddefa2-4723-4203-abe4-9390d79a45f2"
-UUID_MOCK_VALUE_NEW_USERWALLET = "756d5aa2-3f60-4ae8-a9c7-32079d55990d"
-UUID_MOCK_VALUE_NEW_ASSETWALLET = "5bc3d175-513e-43fc-9edd-64c9f6de9b8e"
 
 
 def mock_uuid_wallet():
     return UUID_MOCK_VALUE_NEW_WALLET
 
 
-def put_data_dynamodb_wallet():
-    conn = boto3.client("dynamodb")
-    # create user
-    conn.put_item(
-        TableName=USERS_MOCK_TABLE_NAME,
-        Item={
-            "userId": {"S": UUID_MOCK_VALUE_NEW_USERWALLET},
-            "idNumber": {"S": "00000000A"},
-            "firstName": {"S": "Mary"},
-            "lastName": {"S": "Doe"},
-            "email": {"S": "marydoe@gmail.com"},
-            "phone": {"S": "600000003"},
-        },
-    )
-
-    # create asset
-    conn.put_item(
-        TableName=ASSETS_MOCK_TABLE_NAME,
-        Item={
-            "assetId": {"S": UUID_MOCK_VALUE_NEW_ASSETWALLET},
-            "symbol": {"S": "DOT"},
-            "blockchain": {"S": "Polkadot"},
-        },
-    )
-
-    # create wallets
-    conn.put_item(
-        TableName=WALLETS_MOCK_TABLE_NAME,
-        Item={
-            "walletId": {"S": UUID_MOCK_VALUE_NEW_WALLET1},
-            "address": {"S": "0x123456789"},
-            "balance": {"S": "5"},
-            "userId": {"S": UUID_MOCK_VALUE_NEW_USERWALLET},
-            "assetId": {"S": UUID_MOCK_VALUE_NEW_ASSETWALLET},
-        },
-    )
-    conn.put_item(
-        TableName=WALLETS_MOCK_TABLE_NAME,
-        Item={
-            "walletId": {"S": UUID_MOCK_VALUE_NEW_WALLET2},
-            "address": {"S": "0x987654321"},
-            "balance": {"S": "10"},
-            "userId": {"S": UUID_MOCK_VALUE_NEW_USERWALLET},
-            "assetId": {"S": UUID_MOCK_VALUE_NEW_ASSETWALLET},
-        },
-    )
-
-
 @patch.dict(
     os.environ,
-    {"WALLETS_TABLE": WALLETS_MOCK_TABLE_NAME, "AWS_XRAY_CONTEXT_MISSING": "LOG_ERROR"},
+    {
+        "WALLETS_TABLE": WALLETS_MOCK_TABLE_NAME,
+        "USERS_TABLE": USERS_MOCK_TABLE_NAME,
+        "ASSETS_TABLE": ASSETS_MOCK_TABLE_NAME,
+        "AWS_XRAY_CONTEXT_MISSING": "LOG_ERROR",
+    },
 )
 def test_get_list_of_wallets():
     with my_test_environment():
@@ -375,21 +414,21 @@ def test_get_list_of_wallets():
                 "walletId": UUID_MOCK_VALUE_NEW_WALLET1,
                 "address": "0x123456789",
                 "balance": "5",
-                "userId": UUID_MOCK_VALUE_NEW_USERWALLET,
-                "assetId": UUID_MOCK_VALUE_NEW_ASSETWALLET,
+                "userId": UUID_MOCK_VALUE_MARY,
+                "assetId": UUID_MOCK_VALUE_DOT,
             },
             {
                 "walletId": UUID_MOCK_VALUE_NEW_WALLET2,
                 "address": "0x987654321",
                 "balance": "10",
-                "userId": UUID_MOCK_VALUE_NEW_USERWALLET,
-                "assetId": UUID_MOCK_VALUE_NEW_ASSETWALLET,
+                "userId": UUID_MOCK_VALUE_MARY,
+                "assetId": UUID_MOCK_VALUE_DOT,
             },
         ]
         ret = wallets.lambda_handler(apigw_get_all_wallets_event, "")
-        assert ret["statusCode"] == 200
         data = json.loads(ret["body"])
         assert data == expected_response
+        assert ret["statusCode"] == 200
 
 
 def test_get_single_wallet():
@@ -402,8 +441,8 @@ def test_get_single_wallet():
             "walletId": UUID_MOCK_VALUE_NEW_WALLET1,
             "address": "0x123456789",
             "balance": "5",
-            "userId": UUID_MOCK_VALUE_NEW_USERWALLET,
-            "assetId": UUID_MOCK_VALUE_NEW_ASSETWALLET,
+            "userId": UUID_MOCK_VALUE_MARY,
+            "assetId": UUID_MOCK_VALUE_DOT,
         }
         ret = wallets.lambda_handler(apigw_event, "")
         data = json.loads(ret["body"])
@@ -431,8 +470,22 @@ def test_get_single_wallet_wrong_user_id():
             apigw_event = json.load(f)
             apigw_event["pathParameters"]["userId"] = "123456789"
         ret = wallets.lambda_handler(apigw_event, "")
-        assert json.loads(ret["body"]) == {}
-        assert ret["statusCode"] == 200
+        assert json.loads(ret["body"]) == {"Error": "User not found"}
+        assert ret["statusCode"] == 400
+
+
+def test_add_wallet_wrong_asset_id():
+    with my_test_environment():
+        from src.api import wallets
+
+        with open("./events/wallets/event-post-wallet.json", "r") as f:
+            apigw_event = json.load(f)
+            apigw_event["body"] = json.loads(apigw_event["body"])
+            apigw_event["body"]["assetId"] = "123456789"
+            apigw_event["body"] = json.dumps(apigw_event["body"])
+        ret = wallets.lambda_handler(apigw_event, "")
+        assert json.loads(ret["body"]) == {"Error": "Asset not found"}
+        assert ret["statusCode"] == 400
 
 
 @patch("uuid.uuid1", mock_uuid_wallet)
@@ -450,8 +503,8 @@ def test_add_wallet():
         assert data["address"] == expected_response["address"]
         assert data["balance"] == expected_response["balance"]
         assert data["assetId"] == expected_response["assetId"]
-        assert data["userId"] == expected_response["userId"]
-        assert ret["statusCode"] == 200
+        assert data["userId"] == UUID_MOCK_VALUE_MARY
+        assert ret["statusCode"] == 201
 
 
 def test_delete_wallet():
